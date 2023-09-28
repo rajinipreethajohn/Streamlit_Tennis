9e02b1c1d88c7f56e52ce9d1ec87e0dc544e36ef
RPJOHN <72058664+rajinipreethajohn@users.noreply.github.com>
2023-09-28 13:32:11 +0800

    Updated app.py 28 Sept

--- app.py
+++ app.py
@@ -11,12 +11,21 @@ url = 'https://raw.githubusercontent.com/rajinipreethajohn/Streamlit_Tennis/main
 # Read the CSV file
 df = pd.read_csv(url)
 
+# Format numerical columns as integers
+numerical_columns = ['YEAR', 'WINNER_ATP_RANKING', 'RUNNER-UP_ATP_RANKING', 'WINNER_PRIZE']
+for column in numerical_columns:
+    df[column] = df[column].fillna(0).astype(int)
+
+# Convert "YEAR" column to datetime format and extract the year only
+df['YEAR'] = pd.to_datetime(df['YEAR'], format='%Y').dt.year
+
 st.write("")
 s = df.style.format({"Expense": lambda x : '{:.4f}'.format(x)})
 st.dataframe(s)
 
+# Streamlit app header
 st.title('Tournament and Winner Info')
-fig = px.scatter(df, x='YEAR', y = 'WINNER_NATIONALITY', color="TOURNAMENT", hover_name="WINNER")
+fig = px.scatter(df, x='YEAR', y='WINNER_NATIONALITY', color="TOURNAMENT", hover_name="WINNER")
 fig.update_layout(width=800)
 st.write(fig)
 
@@ -24,11 +33,11 @@ st.write(fig)
 grand_slams = ['Wimbledon', 'U.S. Open', 'French Open', 'Australian Open']
 
 # Filter data for the Grand Slam tournaments
-df_grand_slams = df[df['TOURNAMENT'].isin(grand_slams)]
+df_grand_slams = df[df['TOURNAMENT'].isin(grand_slams)].copy()  # Create a copy of the DataFrame
 
 # Create a custom text column with rotated labels
-df_grand_slams['YEAR'] = df_grand_slams['YEAR'].astype(int)  # Convert YEAR to integers
-df_grand_slams['WINNER_TEXT'] = df_grand_slams['WINNER'] + '<br>' + df_grand_slams['YEAR'].astype(str)
+df_grand_slams.loc[:, 'YEAR'] = df_grand_slams['YEAR'].astype(str)  # Format YEAR as string
+df_grand_slams['WINNER_TEXT'] = df_grand_slams['WINNER'] + '<br>' + df_grand_slams['YEAR']
 
 # Streamlit app header
 st.title('Slide by Years for Details')
@@ -37,7 +46,7 @@ st.title('Slide by Years for Details')
 selected_year = int(st.slider('Select a Year', min_value=1950, max_value=2023, value=2023, step=1))
 
 # Create an interactive line plot based on the selected year
-filtered_df = df_grand_slams[df_grand_slams['YEAR'] <= selected_year]
+filtered_df = df_grand_slams[df_grand_slams['YEAR'].astype(int) <= selected_year]
 
 fig = px.line(filtered_df, x='YEAR', y='WINNER', color='TOURNAMENT',
             title=f'Grand Slam Winners by Year Upto({selected_year})', labels={'WINNER': 'Winner'})
